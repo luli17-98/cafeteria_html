9c0d19cc521fcfd46d46122c4d63a30410984a2d
Luciana Garay <sasluciana123@gmail.com>
2025-11-07 12:35:48 -0300

    Actualización del proyecto: reemplazo de archivos por nueva versión

--- cafeteria_html/app.js
+++ cafeteria_html/app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const conexion = require('./db');
+const conexion = require('../../db');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
